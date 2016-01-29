e862b5ff441abc3cef679094efe8a69b9c23a400
Richard <hello@noxan.net>
2016-01-30 00:30:57 +0100

    install socket.io and combine a simple ping pong server into koa app

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import { Server } from 'http';
 import Koa from 'koa';
+import Socket from 'socket.io';
 
 const app = new Koa();
 
@@ -6,4 +8,15 @@ app.use(async (ctx, next) => {
   ctx.body = 'hello lemming!';
 });
 
-app.listen(3000);
+
+const server = Server(app.callback());
+const io = Socket(server);
+
+io.on('connection', (socket) => {
+  socket.emit('ping', { hello: 'lemming' });
+  socket.on('pong', (data) => {
+    console.log('pong', data);
+  });
+});
+
+server.listen(3000);
