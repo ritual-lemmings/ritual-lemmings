75e220e132db7cae577db847372162b7fd9fe30d
Richard <hello@noxan.net>
2016-01-30 13:29:22 +0100

    move client side javascript code into dedicated file

--- assets/client.js
+++ assets/client.js
@@ -1,1 +1,32 @@
-console.log('bla');
+var socket = io();
+
+socket.on('boot', function (data) {
+  console.log('I am', data.clientType);
+
+  if (data.clientType === 'master') {
+    bootMaster(data);
+  } else {
+    bootSlave(data);
+  }
+});
+
+function bootMaster(data) {
+  socket.on('input', function (data) {
+    console.log('input', data);
+  });
+}
+
+function bootSlave(data) {
+  console.log('My color is', data.clientColor);
+
+  socket.on('game', function (data) {
+    console.log('game', data);
+  });
+}
+
+
+function meep() {
+  console.log('sending meep...');
+  socket.emit('nachricht', { data: 'meep' });
+}
+window.socket = socket;
