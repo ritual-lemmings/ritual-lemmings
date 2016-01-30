95d919b12acd34db9c50e5ba454a079a3117be51
akuryou <akuryou@byteweaver.org>
2016-01-30 19:40:04 +0100

    remove comments

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ var Boot = require('./states/boot');
 var Preload = require('./states/preload');
 var Game = require('./states/game');
 
-//game = new Phaser.Game(1920, 1080, Phaser.AUTO, 'game-container');
-//game = new Phaser.Game(1680, 1050, Phaser.AUTO, 'game-container');
 var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'game-container');
 game.state.add('boot', Boot);
 game.state.add('preload', Preload);
