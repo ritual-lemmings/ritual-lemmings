0050ce145e698ea0a44b04018f32fb9a2308a036
akuryou <akuryou@byteweaver.org>
2016-01-30 19:44:25 +0100

    wrap game into boot mehtod

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-require("../node_modules/phaser/dist/pixi.js");
-require("../node_modules/phaser/dist/phaser.js");
 require('./main.sass');
 
-var Boot = require('./states/boot');
-var Preload = require('./states/preload');
-var Game = require('./states/game');
+window.bootGame = function() {
+  require("../node_modules/phaser/dist/pixi.js");
+  require("../node_modules/phaser/dist/phaser.js");
 
-var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'game-container');
-game.state.add('boot', Boot);
-game.state.add('preload', Preload);
-game.state.add('game', Game);
+  var Boot = require('./states/boot');
+  var Preload = require('./states/preload');
+  var Game = require('./states/game');
 
-game.state.start('boot');
+  var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'game-container');
+  game.state.add('boot', Boot);
+  game.state.add('preload', Preload);
+  game.state.add('game', Game);
+
+  game.state.start('boot');
+};
